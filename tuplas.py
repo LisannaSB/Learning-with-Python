b7e35a72af1ddd85e7fa7380b862a531b606ecba
Lisanna Santiago <lsantiago@phoenixcalibrationdr.com>
2026-08-09 16:27:02 -0400

    modified:   tuplas.py

---
+++ tuplas.py
@@ -0,0 +1,10 @@
+tupla = ('hola', 'mundo', 'somos','tupla') # Definicion de una tupla con elementos
+print(tupla) # Impresion de la tupla con elementos
+print(tupla.count('hola'))
+print(tupla.index('mundo'))
+print(tupla[0])
+
+listaDeTupla = list(tupla) # Conversion de tupla a lista
+print(listaDeTupla) # Impresion de la lista convertida de la tupla
+
+listaDeTupla.append('Chanchito triste') # Agregar un elemento a la lista convertida de la tupla
