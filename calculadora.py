75ec572dfaea71875ca973ef403a77e855b4fa6d
Lisanna Santiago <lsantiago@phoenixcalibrationdr.com>
2026-08-11 15:18:42 -0400

    modified:   calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,20 @@
 primero = input('Ingrese el primer número: ')
 segundo = input('Ingrese el segundo número: ')
 
-print(primero + segundo)
+# print(primero + segundo) # Concatenación de cadenas
+# print(int(primero) + int(segundo)) # Suma de números
+
+try:
+    primero = int(primero) 
+except: # Si no lo logramos
+    primero = 'chanchito feliz'
+
+try:
+    segundo = int(segundo)
+except: # Si no lo logramos
+    segundo = 'chanchito feliz'
+
+if primero == 'chanchito feliz' or segundo == 'chanchito feliz':
+    print('Ingresaste mal los datos, prueba de nuevo solo con números')
+else:
+    print(primero + segundo)
