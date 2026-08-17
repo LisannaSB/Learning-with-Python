a084a3b72160c19f33d80e4df417d1fbb307c1f9
Lisanna Santiago <lsantiago@phoenixcalibrationdr.com>
2026-08-17 11:18:12 -0400

    modified:   Ejercicios_1/ejercicio1_8.py 	modified:   Ejercicios_1/ejercicio1_9.py 	new file:   ejercicio9.txt

--- Ejercicios_1/ejercicio1_8.py
+++ Ejercicios_1/ejercicio1_8.py
@@ -1,7 +1,14 @@
 # Ej 8 Escribir una aplicacion que reciba una cantidad infinita de numeros hasta decir basta, luego que devuelva la suma de los numeros ingresados
 
+result = 0
+numeros = []
 basta = False
 while not basta:
     numerito = input('Introduce un numero: ')
-    if numerito in ('basta'):
+    if numerito not in ('basta'):
+        numeros.append(float(numerito))
+    else:
         basta = True 
+        for n in numeros:
+            result += n
+        print('La suma es de: ', result)
--- Ejercicios_1/ejercicio1_9.py
+++ Ejercicios_1/ejercicio1_9.py
@@ -1,1 +1,17 @@
 # Ej 9 Escribir una funcion que reciba nombre y apellido y los vaya agregando a un archivo
+
+def basedeDatos(nombre, apellido):
+    nombreCompleto = nombre + ' ' + apellido
+    c = open('ejercicio9.txt', 'a')
+    c.write('\n')
+    c.write(nombreCompleto)
+    c.close()
+
+    c = open('ejercicio9.txt')
+    print(c.read())
+    c.close()
+
+nom = input('Dame el nombre: ')
+ape = input('Dame el apellido: ')
+
+basedeDatos(nom, ape)
